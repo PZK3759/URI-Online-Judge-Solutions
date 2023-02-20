ea8a45fe0fffb225e6e26629580c32295cf185a2
Abdullah Al Zahin <118601852+PZK3759@users.noreply.github.com>
2023-02-20 21:40:59 +0600

    Create URI 1113.java

---
+++ Java/URI 1113.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+
+/**
+ *
+ *  @author Abdullah Al Zahin
+ *
+ */
+ 
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner sc = new Scanner(System.in);
+        
+        int x,y;
+        
+        while((x = sc.nextInt()) != (y=sc.nextInt())){
+            
+            if(x>y){
+                System.out.print("Decrescente\n");
+            }
+            else{
+                System.out.print("Crescente\n");
+            }
+            
+        }
+ 
+    }
+ 
+}
