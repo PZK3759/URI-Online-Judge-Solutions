61da82ac4e45bbaec60200ec47f9b3d7603261c1
Abdullah Al Zahin <118601852+PZK3759@users.noreply.github.com>
2023-02-20 20:55:21 +0600

    Create URI 1001.java

---
+++ Java/URI 1001.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ *
+ *  @author Abdullah Al Zahin
+ *
+ */
+
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner sc = new Scanner(System.in);
+        
+        int A, B, X;
+        
+        A = sc.nextInt();
+        B = sc.nextInt();
+        X = A+B;
+        
+        System.out.println("X = " + X);
+ 
+    }
+ 
+}
