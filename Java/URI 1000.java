6dc815b1537e8f9b8c317ef91de8bba0aa9f6163
Abdullah Al Zahin <118601852+PZK3759@users.noreply.github.com>
2023-02-20 20:43:54 +0600

    Create URI 1000.java

---
+++ Java/URI 1000.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+/**
+ * Print Hello World!
+ * @author Zahin
+ */
+public class B1000 {
+    
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.print("Hello World");
+    }
+    
+}
