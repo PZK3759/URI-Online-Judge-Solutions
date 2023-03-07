ee43a765c906c95914aaa811969de69632ef8218
Abdullah Al Zahin <118601852+PZK3759@users.noreply.github.com>
2023-03-07 11:50:31 +0600

    Create URI 1059.java

---
+++ Java/URI 1059.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+
+/**
+ *
+ *  @author Abdullah Al Zahin
+ *
+ */
+ 
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        int i, n=100;
+        
+        for(i= 1; i<=n; i++){
+            if(i%2 == 0){
+                System.out.println(i);
+            }
+        }
+ 
+    }
+ 
+}
